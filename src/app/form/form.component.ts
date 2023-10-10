08d01096bbfebc777636d27a98b8aa070a6bffb1
Hamdardvir Singh <hamdardvir.singh@lotto24.de>
2023-10-10 18:03:53 +0200

    restore form from localstorage

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -7,7 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatStepperModule } from '@angular/material/stepper';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, first, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FormFields, FormState } from './state/form.state';
 import { EditForm, NavigateForm } from './state/form.actions';
@@ -29,7 +29,7 @@ import { EditForm, NavigateForm } from './state/form.actions';
   styleUrls: ['./form.component.scss'],
   standalone: true
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnDestroy {
 
   personalFormGroup = this._fb.group({
     firstname: ['', Validators.required],
@@ -53,21 +53,51 @@ export class FormComponent implements OnInit {
     personal: this.personalFormGroup,
     address: this.addressFormGroup,
     patyment: this.paymentFormGroup
-  })
+  });
 
-  formData$: Observable<FormState>
+  formData$: Observable<FormState>;
+  subscriptions: Subscription = new Subscription();
+  slectectedStep: number = 0;
 
   constructor(private _fb: FormBuilder, private store: Store<{ formdata: FormState }>) {
-    this.formData$ = this.store.select('formdata')
+    this.formData$ = this.store.select('formdata');
   }
 
   ngOnInit(): void {
-  
+    this.subscriptions.add(
+      this.formData$.pipe(
+        first(),
+        tap(data => {
+          this.personalFormGroup.setValue({
+            firstname: data.formfields?.personal?.firstname ?? '',
+            lastname: data.formfields?.personal?.lastname ?? '',
+            phone: data.formfields?.personal?.phone ?? '',
+          })
+
+          this.addressFormGroup.setValue({
+            street: data.formfields?.address?.city ?? '',
+            number: data.formfields?.address?.city ?? '',
+            zip: data.formfields?.address?.city ?? '',
+            city: data.formfields?.address?.city ?? ''
+          })
+
+          this.paymentFormGroup.setValue({
+            name: data.formfields?.payment?.name ?? '',
+            iban: data.formfields?.payment?.iban ?? '',
+          });
+          this.slectectedStep = data.step;
+        }))
+        .subscribe()
+    )
   }
 
   onStepChange(index: number): void {
-    this.store.dispatch(EditForm({ formfields: this.formData.value as FormFields }))
+    this.store.dispatch(EditForm({ formfields: this.formData.value as FormFields }));
     this.store.dispatch(NavigateForm({ step: index }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
